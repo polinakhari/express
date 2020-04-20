aab4f1d98b2baf0fb77d151d99c9ee8e404add3a
polinakhari <polina14.05@yandex.ru>
2020-04-20 21:10:02 +0300

    port

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 5000 
 
 app.get("/", function(req,res) {
     res.send("Hello World")
-});
+});   
 
-app.listen(5000);
+app.listen(PORT);
